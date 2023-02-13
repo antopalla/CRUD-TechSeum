fbbe27f54a0e2899e50505c95cb3a9d11bfa5bf8
TeoSca <scalamatteo04@gmail.com>
2023-02-13 12:51:33 +0100

    Nomi delle sezioni
    Sono stati rinominati le sezioni dalle iniziali al nome completo.

--- back-end_development/create_reperto.php
+++ back-end_development/create_reperto.php
@@ -6,4 +6,9 @@ header('Content-Type: application/json');
 // Connessione al database (require_once = sostituisce la riga di codice con il codice contenuto nel file al path)
 require_once(__DIR__.'/protected/database.php');
 
+
+
+
+
+
 echo '{"status":1, "message":"reperto created"}';
--- back-end_development/get_reperti.php
+++ back-end_development/get_reperti.php
@@ -14,10 +14,19 @@ try{
     $query = $db -> prepare('SELECT * FROM techseum.repertinuova'); // PDO
     $query -> execute();
     $righe_tabella = $query -> fetchAll();
-
+    for($i=0;$i<count($righe_tabella);$i+=1)
+    {
+        if($righe_tabella[$i]['sezione']=="E")
+            $righe_tabella[$i]['sezione']="Elettrotecnica";
+        if($righe_tabella[$i]['sezione']=="I")
+            $righe_tabella[$i]['sezione']="Informatica";
+        if($righe_tabella[$i]['sezione']=="M")
+            $righe_tabella[$i]['sezione']="Meccanica";
+        if($righe_tabella[$i]['sezione']=="S")
+            $righe_tabella[$i]['sezione']="Scienze";
+    }
     echo '{"status":1, "data":'.json_encode($righe_tabella).'}';
     exit();
-
 } catch(PDOException $ex) {
     err("Errore nell'esecuzione della query", __LINE__);
 }
