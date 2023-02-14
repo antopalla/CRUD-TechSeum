95afea73daa42a2624fd16b81ff267bba60a0a54
aledoko <125257006+aledoko@users.noreply.github.com>
2023-02-14 10:35:13 +0100

    Update Crea_Utente.svelte

--- front-end_development/src/components/Crea_Utente.svelte
+++ front-end_development/src/components/Crea_Utente.svelte
@@ -1,10 +1,14 @@
 <script>
     import {TextInput} from 'carbon-components-svelte';
+    import {PasswordInput} from 'carbon-components-svelte';
+    
 </script>
 
 
 <div>
 
-    <TextInput inline labelText="User name" placeholder="Enter user name..." width=50%/>
+    <TextInput inline labelText="User name" placeholder="inserire user name..." width=50%/> <br><br>
+    <PasswordInput inline labelText="Password" placeholder="inserire password..."/><br>
+    <PasswordInput inline placeholder="conferma password..."/>
 
 </div>
