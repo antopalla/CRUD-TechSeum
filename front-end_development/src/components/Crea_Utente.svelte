d15496955896d6b401b321c26a9cccfde1682b44
aledoko <125257006+aledoko@users.noreply.github.com>
2023-02-14 10:42:43 +0100

    Add files via upload

---
+++ front-end_development/src/components/Crea_Utente.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {TextInput} from 'carbon-components-svelte';
+    import {PasswordInput} from 'carbon-components-svelte';
+    
+</script>
+
+
+<div>
+
+    <TextInput inline labelText="User name" placeholder="inserire user name..."/> <br><br>
+    <PasswordInput inline labelText="Password" placeholder="inserire password..."/><br>
+    <PasswordInput inline placeholder="conferma password..."/>
+    
+</div>
