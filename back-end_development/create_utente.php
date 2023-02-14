b6bc9b674905a373f4b76000780fa7dc20d76daf
Gurgo77 <gurgo7377@gmail.com>
2023-02-14 10:25:37 +0100

    get_utente.php, get_utenti.php

--- back-end_development/create_utente.php
+++ back-end_development/create_utente.php
@@ -9,6 +9,11 @@ require_once(__DIR__.'/protected/connessioneDB.php');
 // Per richieste tramite JSON e non tramite FORM utilizzare, in seguito al decommento della seguente riga, $credenziali["username"] $credenziali["password"] $credenziali["nome"] $credenziali["cognome"] $credenziali["amministratore]
 //$credenziali = json_decode(file_get_contents('php://input'), true);
 
+if(!isset($_POST['username']) or !isset($_POST['password']) or !isset($_POST['nome']) or !isset($_POST['cognome'])) {
+    err('Username o password mancanti', __LINE__);
+}
+
+
 // Utilizzo del try - catch per eventuali errori nella query
 try{
     $query = $db -> prepare('INSERT INTO techseum.utenti(username, password, nome, cognome, amministratore) VALUES (:username, :password, :nome, :cognome, :amministratore);'); // PDO
