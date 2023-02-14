0c2a5c282c311c9f3e446b50db3b6ce9db5142f2
aledoko <125257006+aledoko@users.noreply.github.com>
2023-02-14 10:28:32 +0100

    Create Crea_Utente.svelte

---
+++ front-end_development/src/components/Crea_Utente.svelte
@@ -0,0 +1,10 @@
+<script>
+    import {TextInput} from 'carbon-components-svelte';
+</script>
+
+
+<div>
+
+    <TextInput inline labelText="User name" placeholder="Enter user name..." width=50%/>
+
+</div>
