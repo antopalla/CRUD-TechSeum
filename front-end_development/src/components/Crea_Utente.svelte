574a522a2750f37c7b3ca34c8cf52df9fe0b4311
aledoko <125257006+aledoko@users.noreply.github.com>
2023-02-14 12:54:01 +0100

    modifiche

--- front-end_development/src/components/Crea_Utente.svelte
+++ front-end_development/src/components/Crea_Utente.svelte
@@ -1,14 +1,65 @@
 <script>
-    import {TextInput} from 'carbon-components-svelte';
-    import {PasswordInput} from 'carbon-components-svelte';
     
+    import {Button} from 'flowbite-svelte'
+    import {Input} from 'flowbite-svelte'
+
 </script>
 
+<style>
 
-<div>
+.right {
+    position: absolute;
+    right: 10px;
+    width: 600px;
+    border: 0px solid #e6d821;
+    padding: 10px;
+}
+.left{
+    position: absolute;
+    left: 10px;
+    width: 600px;
+    border: 0px solid #e6d821;
+    padding: 10px;  
+}
 
-    <TextInput inline labelText="User name" placeholder="inserire user name..." width=50%/> <br><br>
-    <PasswordInput inline labelText="Password" placeholder="inserire password..."/><br>
-    <PasswordInput inline placeholder="conferma password..."/>
-    
-</div>
+footer { 
+    position: fixed;
+    bottom:40px;
+    right:40px;
+    text-align: center;
+    padding: 3px; 
+}
+
+</style>
+
+<header>
+
+
+
+</header>
+
+<form action="">
+    <section class='right'>
+        
+        <br><Input size="sm" placeholder="Inserire password..." label='Password'/><br><br><br>
+        Conferma password <br><Input size="sm" placeholder="Conferma password..." label="Small input"/><br><br><br>
+    </section>
+
+    <section class='left'>
+        
+        Nome <br><Input size="sm" placeholder="Small input" label="Small input"/>
+        Cognome <br><Input size="sm" placeholder="Small input" label="Small input"/>
+        Username <br><Input size="sm" placeholder="Small input" label="Small input"/>
+        Amministratore <br><Input size="sm" placeholder="Small input" label="Small input"/>
+
+    </section>
+
+
+    <footer>
+        <p >  
+            <Button outline color='yellow'>Crea Utente</Button>
+        </p>
+    </footer>
+
+</form>
+
