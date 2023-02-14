4a5c437b5f4789fa11389026e2a8edf291a25c16
aledoko <125257006+aledoko@users.noreply.github.com>
2023-02-14 19:28:19 +0100

    update crea utente

--- front-end_development/src/components/Crea_Utente.svelte
+++ front-end_development/src/components/Crea_Utente.svelte
@@ -2,64 +2,57 @@
 
     import {TextInput,PasswordInput} from 'carbon-components-svelte';
     import {Checkbox,Button} from 'carbon-components-svelte';
-    
+
     let {checked}=false;
 
 </script>
 
 <style>
 
-    section{
-        position: absolute;
-        right: 200px;     
-        width: 500px;       
+    section{        
+        width: 400px;       
         border: 0px solid #e6d821;
-        padding-top: 50px;
+        padding:50px;
     }
-    .right{
-        right: 200px;     
-        border-right: 1px #e6d821;     
+    header{
+        background-color: #456266;
+        padding:50px;
+        font-size: 35px;
+        color: #b3c5c7;
     }
-
-    .left{
-        left: 200px;   
-        border-left: 1px #e6d821;  
-    }
-
-    /*footer{
-        position: fixed;
-        bottom: 50px;
-        right: 50px;
-        background-color: #d4c821;
-    }*/
-
 
 </style>
 
 
-<br><br>
-
-<form action="localhost/CRUD-TechSeum/back-end_development/create_utente.php" method="post">
-    <section class='left'>
+<center>
+    <form action="localhost/CRUD-TechSeum/back-end_development/create_utente.php" method="post">
         
-        NOME
-        <TextInput placeholder="Inserisci nome..." name='nome'/> <br><br>
-        COGNOME
-        <TextInput placeholder="Inserisci cognome..." name='cognome'/> <br><br>
-        <Checkbox labelText="AMMINISTRATORE" name='amministratore' value='0'/>
-    </section>
+        <header>
+            GESTIONE UTENTI - Creazione
+        </header>
 
-    <section class='right'>
-        USERNAME
-        <TextInput placeholder="Inserisci username..." required name='username'/> <br><br>
-        PASSWORD
-        <PasswordInput placeholder="Inserisci password..." required name='password'/> <br><br>
-        <PasswordInput placeholder="Conferma username..." required/>
-    </section>
+        <div style="display: -webkit-inline-flex;">
+            <section>
+                NOME
+                <TextInput placeholder="Inserisci nome..." name='nome'/> <br><br>
+                COGNOME
+                <TextInput placeholder="Inserisci cognome..." name='cognome'/> <br><br><br>
+                <Checkbox labelText="AMMINISTRATORE" name='amministratore' value='0'/>
+            </section>
 
-    <center style="bottom:50px;">
-        <Button style='background-color:#d4c821;'>Crea Utente</Button>
-    </center>
-
-</form>
-
+            <section>
+                USERNAME
+                <TextInput placeholder="Inserisci username..." required name='username'/> <br><br>
+                PASSWORD
+                <PasswordInput placeholder="Inserisci password..." required name='password'/> <br><br>
+                <PasswordInput placeholder="Conferma username..." required name='conferma'/>
+            </section>
+        </div>
+        <p><Button 
+            style='background-color:#456266;
+                   font-size:20px;
+                   padding:20px'
+            >Crea Utente</Button></p>
+        
+    </form>
+</center>
