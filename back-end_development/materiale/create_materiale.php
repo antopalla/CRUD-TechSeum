1f16ae30eae4adb88cee35e3893f3179c688351b
Gurgo77 <gurgo7377@gmail.com>
2023-02-15 08:57:48 +0100

    Sezione Materiali completata

--- back-end_development/materiale/create_materiale.php
+++ back-end_development/materiale/create_materiale.php
@@ -4,3 +4,20 @@ require_once(__DIR__.'/../protected/headers.php');
 require_once(__DIR__.'/../protected/functions.php');
 require_once(__DIR__.'/../protected/check_session.php');
 require_once(__DIR__.'/../protected/connessioneDB.php');
+
+if(!isset($_POST['NomeMateriale'])) {
+    err('Nome del materiale mancante: ', __LINE__);
+}
+
+// Utilizzo del try - catch per eventuali errori nella query, BIND per evitare SQL INJECTION
+try{
+    $query = $db -> prepare('INSERT INTO  techseum.materiali(codmateriale,nomemateriale) VALUES (NULL, :NomeMateriale)'); // PDO
+    $query -> bindValue(':NomeMateriale', $_POST['NomeMateriale']); 
+    $query -> execute();
+  
+    echo '{"status":1, "data":"Materiale creato"}';
+    exit();
+
+} catch(PDOException $ex) {
+    err("Errore nell'esecuzione della query", __LINE__);
+}
