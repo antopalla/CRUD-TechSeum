20749f1fad2a248a6dc7198166b389641df83345
IMiniMe <105112910+IMiniMe@users.noreply.github.com>
2023-02-17 10:01:48 +0100

    inserimento del logo nella pagina di login

--- front-end_development/src/components/Login_Form.svelte
+++ front-end_development/src/components/Login_Form.svelte
@@ -2,6 +2,7 @@
     import { TextInput, PasswordInput } from 'carbon-components-svelte';
     import { Button } from 'carbon-components-svelte';
     import { current_User, login, loggedIn } from "../js/functions.js";
+    import { ImageLoader, InlineLoading } from "carbon-components-svelte";
 
       //passa i dati all'api check_login
       const form = {
@@ -34,14 +35,31 @@
       font-size: 35px;
       color: #b3c5c7;
     }
+
+    .logo{
+      padding: 0px;
+      width: 115px;
+      height:80px;
+      position:absolute;
+      left:5px;
+      top:10px;
+    }
+  
   
   </style>
-  
   <center>
     <form on:submit|preventDefault={handleForm}>
       <header>
         LOG IN
       </header>
+      <div class="logo">
+        <ImageLoader src="/logo.png">
+          <svelte:fragment slot="loading">
+            <InlineLoading />
+          </svelte:fragment>
+          <svelte:fragment slot="error">An error occurred.</svelte:fragment>
+        </ImageLoader>
+      </div>
       <section>
         USERNAME
         <br><br>
@@ -51,6 +69,8 @@
         <br><br>
         <PasswordInput bind:value={form.password} placeholder="Inserisci password..." required name='password' />
         <br><br>
+        
+     
       </section>
       <p><Button style='background-color:#456266;font-size:20px;padding:10px' type='submit'>Accedi</Button></p>
       
