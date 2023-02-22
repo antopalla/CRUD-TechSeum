c341f371d02d1f0d73bc80a47af1bc2fb3a52025
fradantu21 <93266436+fradantu21@users.noreply.github.com>
2023-02-22 09:58:19 +0100

    Giallo caciocavallo

--- front-end_development/src/components/Reperti_Header.svelte
+++ front-end_development/src/components/Reperti_Header.svelte
@@ -23,9 +23,9 @@
 	let sideNavStyle = "position: absolute ;top: 0px; width: 10%; border-right: 1px solid #161616; transition: transform 300ms ease-in;"
 	let buttonStyle="align-items: center; padding: 10px; color:#161616 ;"
 	let toggleStyle= "padding-left:10px ; justify-content: center; padding-bottom: 12px;"
-	let bgColor = "#e0e0e0"
+	let bgColor = "#f1c40e"
+	let menuBgColor = "#e0e0e0"
 	let text_color = "#161616"
-	let title_shadow_color = "#00000055";
 	let menu_icon = Menu
 
 	function redirectUtenti() {
@@ -37,9 +37,7 @@
 		}
     }
 
-	function redirectAggReperto() {
-		goto("/reperti/aggiungi_reperto")
-	}
+	
 
 	let isSideNavOpen = false
 
@@ -50,14 +48,14 @@
 			buttonStyle = "align-items: center; padding: 10px; color:#c6c6c6 ;"
 			bgColor = "#393939"
 			text_color = "#c6c6c6"
-			title_shadow_color = "#ffffff55"
+			menuBgColor = "#393939"
 		}
 		else{
 			theme = "white"
 			buttonStyle = "align-items: center; padding: 10px; color:#161616 ;"
-			bgColor = "#e0e0e0"
+			bgColor = "#f1c40e"
+			menuBgColor = "#e0e0e0"
 			text_color = "#161616"
-			title_shadow_color = "#00000055"
 		}		
 	}
 
@@ -66,7 +64,7 @@
 <Theme bind:theme /> <!-- permette al sito di cambiare tema in tempo reale -->
 
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=Phudu:wght@900&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@600&display=swap');
 	.container{
 		justify-content: center;
 		display: flex;
@@ -78,27 +76,27 @@
 	}
 	.logo{
 		position: absolute;
-		left: 10%;
+		right: 10%;
 		padding: 0px;
-		width: 100px;
-		height: 100px;
+		width: 50px;
+		height: 50px;
 	}
 	.menu_title{
-		font-family: 'Phudu', cursive;
+		font-family: 'Josefin Sans', sans-serif;
 		height: 84px;
 	}
 	.header_title{
-		height: 100px;
+		height: 50px;
 		display: flex;
 		align-items: center;
-		font-family: 'Phudu', cursive;
-		font-size: 4em ;	
+		font-family: 'Josefin Sans', sans-serif ;
+		font-size: 2em ;	
 	}
 </style>
 
 <SideNav 
 	bind:isOpen={isSideNavOpen}
-	style={sideNavStyle + "background-color: "+ bgColor}
+	style={sideNavStyle + "background-color: "+ menuBgColor}
 	>
   <SideNavItems>
 	<center><h1 class="menu_title" style="color: {text_color};">MENU</h1></center> 
@@ -110,7 +108,7 @@
 			labelB = "Dark mode"
 			on:toggle = {darkModeHandler}	
 		/>
-	<Button icon = {Add} kind = "ghost" style={buttonStyle} on:click={redirectAggReperto}> Aggiungi reperti </Button>
+	
     <Button icon = {Person} kind = "ghost" style={buttonStyle + "width : 100%"} on:click={redirectUtenti}> Utenti </Button>
   </SideNavItems>	
 </SideNav>
@@ -126,12 +124,12 @@
 				<svelte:fragment slot="error">An error occurred.</svelte:fragment>
 			</ImageLoader>
 		</div>
-		<h1 class="header_title" style="text-shadow: 2px 2px {title_shadow_color};" >TechSeum</h1>	
+		<h1 class="header_title" >DI MAGGIO •TECHSEUM</h1>	
 		<Button 
 			icon = {menu_icon}
 			iconDescription = "Menu"
 			kind = "ghost"
-			style = {buttonStyle +" position: absolute ; right: 10% ; height : 100px ; margin: 0px"}
+			style = {buttonStyle +" position: absolute ; left: 10% ; height : 50px ; margin: 0px"}
 			on:click = {()=>{
 				isSideNavOpen = !isSideNavOpen;
 				if(!isSideNavOpen){menu_icon = Menu;}
