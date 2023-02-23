a480b2c42c55ec1da74f0a3ffc3e4d27be82f544
Gurgo77 <gurgo7377@gmail.com>
2023-02-23 08:17:15 +0100

    Update delete_utente.php

--- back-end_development/utente/delete_utente.php
+++ back-end_development/utente/delete_utente.php
@@ -1,20 +1,22 @@
 <?php
+// API PER LA RIMOZIONE DI UN UTENTE DAL DATABASE
 
 require_once(__DIR__.'/../protected/headers.php');
 require_once(__DIR__.'/../protected/functions.php');
 require_once(__DIR__.'/../protected/check_session.php');
 require_once(__DIR__.'/../protected/connessioneDB.php');
 
-// Per richieste tramite JSON e non tramite FORM utilizzare, in seguito al decommento della seguente riga, $credenziali["username"] $credenziali["password"] $credenziali["nome"] $credenziali["cognome"] $credenziali["amministratore]
+// Per richieste tramite JSON e non tramite FORM utilizzare, in seguito al decommento della seguente riga, $credenziali["username"] $credenziali["password"] 
 // $credenziali = json_decode(file_get_contents('php://input'), true);
 
 /// Utilizzo del try - catch per eventuali errori nella query, BIND per evitare SQL INJECTION
 try{
-    $query = $db -> prepare('DELETE FROM techseum.utenti WHERE username=:username AND password=:password;'); // QUERY DI ELIMINAZIONE DELL' UTENZA
+    $query = $db -> prepare('DELETE FROM techseum.utenti WHERE username=:username AND password=:password;'); 
     $query -> bindValue(':username', $_POST['username']); 
     $query -> bindValue(':password', $_POST['password']); // --> Fare md5 da SvelteKIT
     $query -> execute();
   
+    // Output dell'API in formato JSON
     echo '{"status":1, "data":"Utenza cancellata"}';
     exit();
 
