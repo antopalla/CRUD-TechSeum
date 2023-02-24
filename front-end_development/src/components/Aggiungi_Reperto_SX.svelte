c128df36ef2a2945dfc760de34ab5d471fb57410
Gurgo77 <gurgo7377@gmail.com>
2023-02-24 09:42:12 +0100

    indentazione

--- front-end_development/src/components/Aggiungi_Reperto_SX.svelte
+++ front-end_development/src/components/Aggiungi_Reperto_SX.svelte
@@ -1,101 +1,90 @@
 <script>
   import { Column, Grid, TextArea } from "carbon-components-svelte";
 
+//Funzione per la visualizzazione dell'immagine di copertina
+function previewCoverImage(event) {
+    var reader = new FileReader();
+    reader.onload = function() {
+      var output = document.getElementById('cover-image-preview');
+      output.src = reader.result;
+      output.style.height='200px'
+    }
+    reader.readAsDataURL(event.target.files[0]);
+  }
 
-  function previewCoverImage(event) {
-			var reader = new FileReader();
-			reader.onload = function() {
-				var output = document.getElementById('cover-image-preview');
-				output.src = reader.result;
-        output.style.height='200px'
-			}
-			reader.readAsDataURL(event.target.files[0]);
-		}
-
-    function previewGalleryImages(event) {
-      var previewContainer = document.getElementById('gallery-images-preview');
-      previewContainer.innerHTML = '';
-
-      var files = event.target.files;
-      for (var i = 0; i < files.length; i++) {
+  //Funzione per la visualizzazione delle immagini della galleria
+function previewGalleryImages(event) {
+    var previewContainer = document.getElementById('gallery-images-preview');
+    previewContainer.innerHTML = '';
+    var files = event.target.files;
+    for (var i = 0; i < files.length; i++) {
       var file = files[i];
       var reader = new FileReader();
       reader.onload = (function(file) {
-      return function() {
-        var img = document.createElement('img');
-        img.src = this.result;
-        img.style.height='100px';
-        img.style.width='100px';
-        previewContainer.appendChild(img);
-      };
-    })(file);
-
-    reader.readAsDataURL(file);
-  }
+        return function() {
+          var img = document.createElement('img');
+          img.src = this.result;
+          img.style.height='100px';
+          img.style.width='100px';
+          previewContainer.appendChild(img);
+        };
+      })(file);
+      reader.readAsDataURL(file);
+    }
 }
-
-      
-
 
 </script>
 
 <style>
+.did1{
+    float: left;
+    margin-top: 5%;
+    margin-left: 35%;
+    width: 200px;
+    position:absolute; 
+}
 
-  .did1{
-      float: left;
-      margin-top: 5%;
-      margin-left: 35%;
-      width: 200px;
-      position:absolute;
-      
-      
-  }
+.did2{
+    margin: right;
+    margin-top: 17%;
+    margin-left: 35%;
+    width: 200px;
+    position: absolute;
+}
 
-  .did2{
-      margin: right;
-      margin-top: 17%;
-      margin-left: 35%;
-      width: 200px;
-      position: absolute;
-  }
-
-  .prove{
-      margin: left;
-      margin-top: 5%;
-      margin-left: 10%;
-      width: 300px;
-      height: 300px;
-      position: absolute;
-  }
-
-
+.prove{
+    margin: left;
+    margin-top: 5%;
+    margin-left: 10%;
+    width: 300px;
+    height: 300px;
+    position: absolute;
+}
 </style>
 
+<!-- Inserimento delle immagini -->
 <div class='prove'>
   <label for="cover-image">Immagine di copertina:</label><br>
   <input type="file" id="cover-image" name="cover-image" accept="image/*" on:change={previewCoverImage}><br><br>
   <!-- svelte-ignore a11y-missing-attribute -->
   <img id="cover-image-preview"><br><br>
-
   <label for="gallery-images">Galleria di immagini:</label><br>
   <input type="file" id="gallery-images" name="gallery-images" accept="image/*" on:change={previewGalleryImages} multiple><br><br>
-  
   <div id="gallery-images-preview"></div><br><br>   
-
 </div>
 
 <div class="did1">
-<TextArea 
-  rows={5}
-  labelText="Didascalia:"
-  placeholder="Inserire didascalia reperto..."
-/>
+  <TextArea 
+    rows={5}
+    labelText="Didascalia:"
+    placeholder="Inserire didascalia reperto..."
+  />
 </div>
 
 <div class="did2">
-<TextArea 
-  rows={5}
-  labelText="Denominazione storica:"
-  placeholder="Inserire denomnazione storica reperto..."
-/>
+  <TextArea 
+    rows={5}
+    labelText="Denominazione storica:"
+    placeholder="Inserire denomnazione storica reperto..."
+  />
 </div>
