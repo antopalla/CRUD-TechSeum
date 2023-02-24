eeefa64a84c2f5e76ee6997cb9f0445b741ae7b4
TeoSca <scalamatteo04@gmail.com>
2023-02-24 10:57:03 +0100

    Sostituzione di get con post nei back end
    sostituzione e array nel create_reperto

---
+++ back-end_development/reperto/tempCodeRunnerFile.php
@@ -0,0 +1,5 @@
+<?php
+$querio=$db -> prepare('INSERT INTO techseum.comGEToda(codassoluto,codmateriale) VALUES (:codassoluto,:codmateriale);');
+        // $querio -> bindValue(':codmateriale', $_GET['codmateriale'][$i]);
+        // $querio -> bindValue(':codassoluto', $codassoluto);
+        // $querio -> execute();
